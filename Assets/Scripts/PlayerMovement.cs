87e3b6467eb765cd0da5909b50b8a370b10798b3
Jakob-Nilsson <kc.jakob.nilsson@gmail.com>
2026-02-27 23:11:54 +0100

    nytt player script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,25 +7,42 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D _rb2D;
     [SerializeField]
-    private Vector2 kontroll = new Vector2(0, 0);
+    private Vector2 kontroll;
     [SerializeField]
-    private float speed = 100f;
-    
+    private float speed = 10f;
+    [SerializeField]
+    private KeyCode left;
+    [SerializeField]
+    private KeyCode right;
+    private float direction;
+    private KeyCode input;
 
-/*    private void FixedUpdate()
-    {
-        _rb2D.velocity = kontroll;
-    }
-*/
+   
+
    
     void Update()
     {
+        if (!Input.anyKey)
+        {   
+            direction = 0;
+
+        }
         
-        float input = Input.GetAxisRaw("Horizontal");
-        kontroll = new Vector2(input, 0);
-        transform.Translate(kontroll * speed * Time.deltaTime);
-
+        if (Input.GetKey(left))
+        {
+            direction = -1f;
+        }
+        if (Input.GetKey(right))
+        {
+            direction = 1f;
+        }
+          
+        
+    } 
+    void FixedUpdate()
+    {
+        _rb2D.velocity = new Vector2(direction * speed, _rb2D.velocity.y);
     }
-
-
 }
+
+
